412a7f0cdc9e07aa76e0f04786763aedd96f5f22
akito0802 <light.of.king.a@gmail.com>
2026-07-30 12:12:36 +0900

    Add Other tools link to shared menu

--- shared-menu.js
+++ shared-menu.js
@@ -9,6 +9,7 @@
       <a class="shared-menu-link" href="https://akito0802.github.io/scale/"><span class="shared-menu-icon">🎸</span><span>スケール</span></a>
       <a class="shared-menu-link" href="https://akito0802.github.io/-h/"><span class="shared-menu-icon">🎵</span><span>指板</span></a>
       <a class="shared-menu-link current" href="index.html"><span class="shared-menu-icon">📚</span><span>コード表</span></a>
+      <a class="shared-menu-link" href="https://akito0802.github.io/NEET-note/tools.html"><span class="shared-menu-icon">🧰</span><span>その他</span></a>
     </nav>
     <button id="sharedMenuOpenBtn" class="shared-menu-button" type="button" aria-label="メニューを開く" aria-expanded="false">☰</button>`;
   document.body.prepend(wrapper);
